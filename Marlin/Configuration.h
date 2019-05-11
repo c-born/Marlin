8f8c1f68cdef3200edff6d3b38ed62183ebdaf75
David Varley <DavidAVarley@gmail.com>
2019-05-11 13:29:40 +1000

    Tried going back to BILINEAR, doesn't work, 1.5mm between head and bed at Z=0. Seems to be an ongoing bug from around 1.1.8? Going to try the 2.0.x branch, else go back to the previous working 1.1.1

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -985,15 +985,15 @@
  */
 //#define AUTO_BED_LEVELING_3POINT
 //#define AUTO_BED_LEVELING_LINEAR
-#define AUTO_BED_LEVELING_BILINEAR
-//#define AUTO_BED_LEVELING_UBL
+#define AUTO_BED_LEVELING_BILINEAR // Use this up until now (10MAY19) DAV - and back!
+//#define AUTO_BED_LEVELING_UBL // Tried this. To complicated to set up, didn't go well, maybe try again later?
 //#define MESH_BED_LEVELING
 
 /**
  * Normally G28 leaves leveling disabled on completion. Enable
  * this option to have G28 restore the prior leveling state.
  */
-//#define RESTORE_LEVELING_AFTER_G28
+#define RESTORE_LEVELING_AFTER_G28
 
 /**
  * Enable detailed logging of G28, G29, M48, etc.
